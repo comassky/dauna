5e65b324e3d9d45b6f0d1e3ad99d7664317c1f76
comasSky <jacquot.hadrien@gmail.com>
2021-04-28 17:33:34 +0200

    Premier commit

---
+++ src/main/java/com/comassky/dauna/DaunaApplication.java
@@ -0,0 +1,11 @@
+package com.comassky.dauna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaunaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DaunaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/comassky/dauna/DaunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comassky.dauna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
